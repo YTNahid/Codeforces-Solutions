bd851852b42b19ca9dfd6ecb42fa93a31f2cc11b
YTNahid <65461710+YTNahid@users.noreply.github.com>
2022-11-18 16:08:15 +0600

    Create 50A - Domino piling.c

---
+++ 50A - Domino piling.c
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/50/A
+#include <stdio.h>
+
+int main() {
+    
+    int m, n, d = 2, b;
+    scanf("%d %d", &m, &n);
+    b = m * n;
+    printf("%d", b / d);
+
+}
