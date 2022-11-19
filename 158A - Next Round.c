32607554a712849b78ccb56dea27159e4ea5ddda
YTNahid <65461710+YTNahid@users.noreply.github.com>
2022-11-20 02:36:23 +0600

    Update 158A - Next Round.c

--- 158A - Next Round.c
+++ 158A - Next Round.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
  
 int main(){
-    
-    int a, b, count = 0, i, zero = 0;
+
+    int a, b, count = 0, i;
     scanf("%d%d", &a, &b);
 
     int arr[a];
@@ -11,18 +11,11 @@ int main(){
         scanf("%d", &arr[i]);
     }
 
-    for(i = b-1; i >= 0; i--) {
-        if(arr[i] != 0) {
+    for(i = 0; i < a; i++) {
+        if(arr[i] >= arr[b-1] && arr[i] > 0) {
             count++;
         }
     }
-    if(arr[b-1] > 0) {
-        for(i = 0; i < a - b; i++) {
-            if(arr[b-1] == arr[b+i]) {
-            count++;
-            }
-        }
-    }
-    printf("%d\n", count);
+    printf("%d", count);
   
 }
