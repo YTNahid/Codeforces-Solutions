5f70413aaf693f226b4c4e5d80ec15e45d48c3a2
YTNahid <65461710+YTNahid@users.noreply.github.com>
2022-11-26 19:42:06 +0600

    Create 228A - Is your horseshoe on the other hoof?.c

---
+++ 228A - Is your horseshoe on the other hoof?.c
@@ -0,0 +1,27 @@
+// https://codeforces.com/problemset/problem/228/A
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    long arr[4],i,j, count = 0;
+
+    for(i = 0; i < 4; i++) {
+        scanf("%ld", &arr[i]);
+    }
+
+    for(i = 0; i < 3; i++) {
+        for(j = 3; j >= 1+i; j--) {
+            if(arr[i] == arr[j]) {
+                count++;
+                arr[j] = -j-i;
+            }
+        }
+    }
+    printf("%d", count);
+         
+}
